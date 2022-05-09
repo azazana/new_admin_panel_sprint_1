ca13dcbd0423123228b85e393e361e755a19cfcc
azazana <aazazana@gmail.com>
2022-05-09 09:53:48 +0300

    add testing

--- 03_sqlite_to_postgres/loader_saver.py
+++ 03_sqlite_to_postgres/loader_saver.py
@@ -27,9 +27,7 @@ class SQLiteLoader:
             curs = self.conn.cursor()
             query = f'SELECT * FROM {table};'
             curs.execute(query)
-            while True:
-                data = curs.fetchmany(self.size)
-
+            while data := curs.fetchmany(self.size):
                 list_data = [cls(*i) for i in data]
                 if not data:
                     break
@@ -47,26 +45,28 @@ class PostgresSaver:
         self.size = size
         self.cursor = self.connect.cursor()
 
-    def truncate(self, table):
-        self.cursor.execute(f"TRUNCATE {table} CASCADE;")
+    # def truncate(self, table):
+    #     self.cursor.execute(f"TRUNCATE {table} CASCADE;")
 
     def save_all_data(self, data_dict: dict):
         for table, data_gen in data_dict.items():
-            self.truncate(table)
+            # self.truncate(table)
             counter = 0
             for list_data in data_gen:
 
                 dict_cls = asdict(list_data[0])
                 keys_sql = ', '.join(dict_cls.keys())
+                set_keys = ", ".join([i + ' = EXCLUDED.' + i for i in dict_cls.keys()])
                 count_s = ('%s, ' * len(dict_cls.keys()))[:-2]
                 query = (f'INSERT INTO {table} ({keys_sql}) VALUES ({count_s}) '
-                         f' ON CONFLICT (id) DO NOTHING;')
+                         f' ON CONFLICT (id) DO UPDATE'
+                         f' SET {set_keys};')
                 tuple_butch = (astuple(dicts) for dicts in list_data)
                 try:
                     self.cursor.executemany(query, tuple_butch)
-                except(Exception):
-                    logger.error('Error of downloading data in table %s', table)
-                counter += len(dict_cls)
+                except(Exception) as err:
+                    logger.error('Error of downloading data in table %s. Error %s', table, err)
+                counter += len(list_data)
             logger.info('Data in table %s is downloaded, count of rows %s', table, counter)
 
         self.connect.commit()
